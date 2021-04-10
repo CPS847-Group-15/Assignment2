295e0539755e3e07a121923dbf68c12001a38816
William Pena Ayala <william.pena@ryerson.ca>
2021-04-10 16:29:10 -0400

    Unittest test

--- app/src/fizzbuzz.py
+++ app/src/fizzbuzz.py
@@ -1,4 +1,6 @@
 def fb(number):
+    if(number == 0):
+        return "0"
     if(number % 15 == 0):
         return "fizz buzz"
     if(number % 5 == 0):
--- app/src/unittest.py
+++ app/src/unittest.py
@@ -1,2 +1,34 @@
+import unittest
+from app.src.fizzbuzz import fb
+
+class TestFizzBuzz(unittest.TestCase):
+    def test_fizz(self):
+        self.assertEqual(fb(3), "fizz", "Should return 'fizz'")
+        self.assertEqual(fb(12), "fizz", "Should return 'fizz'")
+        self.assertEqual(fb(18), "fizz", "Should return 'fizz'")
+        self.assertEqual(fb(306), "fizz", "Should return 'fizz'")
+        self.assertEqual(fb(903), "fizz", "Should return 'fizz'")
+
+    def test_buzz(self):
+        self.assertEqual(fb(5), "buzz", "Should return 'buzz'")
+        self.assertEqual(fb(20), "buzz", "Should return 'buzz'")
+        self.assertEqual(fb(25), "buzz", "Should return 'buzz'")
+        self.assertEqual(fb(10), "buzz", "Should return 'buzz'")
+        self.assertEqual(fb(35), "buzz", "Should return 'buzz'")
+        self.assertEqual(fb(10010), "buzz", "Should return 'buzz'")
+
+    def test_zero(self):
+        self.assertEqual(fb(0), "0", "Should return '0'")
+
+    def test_numbers(self):
+        self.assertEqual(fb(1), "1", "Should return '1'")
+        self.assertEqual(fb(2), "2", "Should return '2'")
+        self.assertEqual(fb(4), "4", "Should return '4'")
+        self.assertEqual(fb(22), "22", "Should return '22'")
+        self.assertEqual(fb(16), "16", "Should return '16'")
+        self.assertEqual(fb(64), "64", "Should return '64'")
+        self.assertEqual(fb(164), "164", "Should return '164'")
+
 if __name__ == "__main__":
     print("Hello World!")
+    unittest.main()
